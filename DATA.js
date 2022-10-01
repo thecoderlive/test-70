660d0cfa6a19abe59607d36745d9ef872f65903e
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-01 14:42:44 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "2a9abbec-57f3-44a1-8a67-af2cc450132a",
+    "home_feed": [
+        {
+            "id": "a62395ee-c92c-4597-b87c-f1ad3ea552a5",
+            "artist_name": "treatment",
+            "home_carousel": [
+                {
+                    "id": "bdd652aa-3c81-4556-932f-5de274cdc8d5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                },
+                {
+                    "id": "2fd03c48-dd7a-4437-ade8-0347722b1fa4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png"
+                },
+                {
+                    "id": "7831b92f-e16f-40dc-88f9-fa454d1fdbb8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "81600015-415e-4746-955d-02ad0183a15b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "3081c25c-ea7d-4cf9-b8f0-f4a8b41fbb88",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                }
+            ],
+            "art_caption": "thousand",
+            "art_description": "It anything effort many spring consumer administration."
+        },
+        {
+            "id": "59e618b0-608d-4eb4-bc76-117afad9ce88",
+            "artist_name": "action",
+            "home_carousel": [
+                {
+                    "id": "21e19b8e-ff27-46b7-8c37-77cac8c8778a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/207.png"
+                },
+                {
+                    "id": "b24b3570-d311-46f6-8522-3372c5e0836e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                },
+                {
+                    "id": "4d0128dd-8e28-467d-9322-326c12ad0598",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "b24afb8e-df2d-4ef1-a5b3-497836b287ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "de548df0-369f-47b7-9e60-d2bc0699894f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/314.png"
+                }
+            ],
+            "art_caption": "speech",
+            "art_description": "Yes full cup industry. War market hair tend radio form hope. Drug significant able wall specific tell threat."
+        },
+        {
+            "id": "ca199ab3-641c-46cb-9b2d-c1e16d438920",
+            "artist_name": "language",
+            "home_carousel": [
+                {
+                    "id": "804b61ec-a367-43da-b36c-75a5e7b9d133",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                },
+                {
+                    "id": "f624104e-a60e-41b0-9769-d85996ffe296",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png"
+                },
+                {
+                    "id": "5009e644-b0b2-48dd-a4db-45236796fa04",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "d4cba6dc-0c78-4024-86bc-eadcfa7b8b78",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "2250646e-e72e-4d56-bfa2-fef9ae6c9825",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png"
+                }
+            ],
+            "art_caption": "she",
+            "art_description": "Five father security leader. Investment off agent certainly full statement people. Way cover music of resource trial."
+        },
+        {
+            "id": "27c6bfe6-55cc-44ab-8430-f77bcc7bb76e",
+            "artist_name": "entire",
+            "home_carousel": [
+                {
+                    "id": "f2114542-5a4e-41fe-bddf-c42c08c0d779",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "a7456602-76cc-4728-b309-abafd0c1f8f6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                },
+                {
+                    "id": "a68b5da1-025b-48c7-bdf1-2d99dec57ac6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "035d6036-de06-473f-8703-b8a7892062b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                },
+                {
+                    "id": "24f78a55-ef91-4713-87bd-9a8a72c2f8d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                }
+            ],
+            "art_caption": "most",
+            "art_description": "Rise exist nearly game serve. Vote money throw soon."
+        },
+        {
+            "id": "04c39d28-9761-4abd-93e1-24891292ab63",
+            "artist_name": "energy",
+            "home_carousel": [
+                {
+                    "id": "63c937e8-3270-47f9-8a87-7c6368a5fcf7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "068e52dc-5c35-4005-af92-73c623efc81b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                },
+                {
+                    "id": "ddc2afad-8c5d-470a-beb7-98bbb58773b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                },
+                {
+                    "id": "b850559e-84e6-43bd-ad22-f2050adcf141",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                },
+                {
+                    "id": "3784941e-8f59-4e99-b4b9-9f0a77d3b213",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                }
+            ],
+            "art_caption": "world",
+            "art_description": "Market good lay media tend by. Such produce structure collection direction first great."
+        }
+    ]
+}
